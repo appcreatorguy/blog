bebbd5df47ce78707675fb96268e204431eee367
Manas Mengle <manmenonsense@gmail.com>
2024-07-03 16:18:00 +0100

    smarter opt out link text

--- scripts/optOut.js
+++ scripts/optOut.js
@@ -1,13 +1,21 @@
 globalThis.onload = function () {
     const a = document.getElementById("optOut");
 
+    if (localStorage.getItem("umami.disabled") === "1") {
+        a.innerText = "opt in";
+    } else {
+        a.innerText = "opt out";
+    }
+
     a.onclick = function () {
       if (localStorage.getItem("umami.disabled") === "1") {
         localStorage.removeItem("umami.disabled");
         console.log("umami enabled!");
+        a.innerText = "opt out";
       } else {
         localStorage.setItem("umami.disabled", 1);
         console.log("umami disabled!");
+        a.innerText = "opt in";
       }
     };
 };
